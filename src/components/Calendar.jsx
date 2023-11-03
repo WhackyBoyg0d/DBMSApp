fac7804a8c01300ba77ed58de54803790d15df8b
Aditya Aggarwal <85567110+WhackyBoyg0d@users.noreply.github.com>
2023-11-03 18:25:42 +0800

    ft- sidebar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,13 +2,28 @@ import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import {useState} from 'react';
 
 export default function BasicDateCalendar() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  // console.log(date);
+  
+
   return (
     <div className="max-w p-6 bg-white shadow rounded-lg ">
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateCalendar  
+      defaultValue = {selectedDate}
+      onChange={(newValue) => {
+        setSelectedDate(newValue);
+      }}
+      />
     </LocalizationProvider>
+    <div>
+      <p>
+        Current Date - {selectedDate?.toString() || 'none'}
+      </p>
+    </div>
     </div>
     
   );
