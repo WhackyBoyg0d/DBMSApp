208668eeb54ec0a89f950a7d2508e490411389d7
Aditya Aggarwal <85567110+WhackyBoyg0d@users.noreply.github.com>
2023-11-23 19:09:17 +0800

    yay

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,12 +4,10 @@ import Sidebar from './Sidebar'
 import Welcome from './Welcome'
 import UpcomingLecture from './UpcomingLecture'
 import Timetable from './Timetable'
-import TestLecture from '../stash/TestLecture'
 
 
 const Dashboard = () => {
-  // const lectures = JSON.parse(localStorage.getItem("timeTable"));
-  // console.log(lectures);
+  
   return (
     
       
@@ -22,16 +20,14 @@ const Dashboard = () => {
             <Sidebar/>
           </div>  
           <div className="w-2/3 mr-4 flex flex-col h-screen ">
-            {/* <UpcomingClasses /> */}
+          
             <Welcome />
-            {/* <UpcomingLecture /> */}
+        
             <Timetable />
           </div>
           <div className="w-1/3 flex flex-col h-screen">
             <UpcomingLecture />
-            {/* <Welcome /> */}
-            {/* <Calendar /> */}
-            {/* <TestLecture lectures={lectures}/> */}
+      
             
           </div>
         </div>
